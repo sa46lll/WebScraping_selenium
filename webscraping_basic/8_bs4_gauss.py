e399fabddef047d714b0e69ba0e9f6e13aca2ac2
sa46lll <9812smh@naver.com>
2021-06-14 12:20:52 +0900

    Gauss_rating

--- webscraping_basic/8_bs4_gauss.py
+++ webscraping_basic/8_bs4_gauss.py
@@ -8,13 +8,23 @@ res.raise_for_status()
 soup = BeautifulSoup(res.text, "lxml")
 
 cartoons = soup.find_all("td", attrs={"class" : "title"})
+ratings = soup.find_all("div", attrs={"class" : "rating_type"})
 # title = cartoons[0].a.get_text()
 # link = cartoons[0].a["href"]
 # print(title)
 # print("https://comic.naver.com" + link)
 
-# 가우스 웹툰의 모든 화
+# 만화 제목 + 링크 가져오기
 for cartoon in cartoons:
     title = cartoon.a.get_text()
     link = "https://comic.naver.com" + cartoon.a["href"]
     print(title, link)
+
+# 평점 구하기
+total_rates = 0
+for rating in ratings:
+    rate = rating.find("strong").get_text() # rating.strong.get_text()
+    print(rate)
+    total_rates += float(rate)
+print("전체 점수 : ", total_rates)
+print("평균 점수 : ", total_rates / len(ratings))
