6450e4dbd62cc9fe6683f8cd0a97ac9648371ab1
sa46lll <9812smh@naver.com>
2021-06-16 01:13:09 +0900

    Save stock as csv file

--- webscraping_basic/12_csv_stock.py
+++ webscraping_basic/12_csv_stock.py
@@ -4,7 +4,16 @@ from bs4 import BeautifulSoup
 
 url = "https://finance.naver.com/sise/sise_market_sum.nhn?sosok=0&page="
 
-for page in range(1, 2):
+# csv 파일 저장
+filename = "시가총액1-200.csv"
+f = open(filename, "w", encoding="utf8", newline="") # 한글이 깨질때 encoding="utf-8-sig"
+writer = csv.writer(f)
+
+# column 이름
+title = "N	종목명	현재가	전일비	등락률	액면가	시가총액	상장주식수	외국인비율	거래량	PER	ROE".split("/t")
+writer.writerow(title)
+
+for page in range(1, 5):
     res = requests.get(url + str(page))
     res.raise_for_status()
     soup = BeautifulSoup(res.text, "lxml")
@@ -16,3 +25,5 @@ for page in range(1, 2):
             continue
         data = [column.get_text().strip() for column in columns] # strip : 공백 삭제
         print(data)
+        writer.writerow(data)
+
