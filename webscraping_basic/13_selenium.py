c7209b1f230ec602241e816feca26765de1b3ce1
sa46lll <9812smh@naver.com>
2021-06-21 13:19:35 +0900

    sleep()

--- webscraping_basic/13_selenium.py
+++ webscraping_basic/13_selenium.py
@@ -1,13 +1,45 @@
 from selenium import webdriver
+import time
 
-options = webdriver.ChromeOptions()
-options.add_experimental_option("excludeSwitches", ["enable-logging"])
-browser = webdriver.Chrome(options=options) # chromedriver 경로 지정 ("./chromedriver.exe")
+# options = webdriver.ChromeOptions()
+# options.add_experimental_option("excludeSwitches", ["enable-logging"])
+# browser = webdriver.Chrome(options=options) # chromedriver 경로 지정 ("./chromedriver.exe")
 
+
+browser = webdriver.Chrome("z:/nadoPython/chromedriver")
+
+# 1. 네이버 이동
 url = "http://naver.com"
 browser.get(url)
-while(True):
-    pass 
+time.sleep(3)
+
+# 2. 로그인 버튼 클릭
+elem = browser.find_element_by_class_name("link_login")
+elem.click()
+
+# 3. id, pw 입력
+browser.find_element_by_id("id").send_keys("naver_id")
+browser.find_element_by_id("pw").send_keys("password")
+
+# 4. 로그인 버튼 클릭
+browser.find_element_by_id("log.login").click()
+time.sleep(3)
+
+# 5. id 를 새로 입력
+browser.find_element_by_id("id").clear()
+browser.find_element_by_id("id").send_keys("my_id")
+time.sleep(3)
+
+# 6. html 정보 출력
+print(browser.page_source)
+
+# 7. 브라우저 종료
+# browser.close() # 현재 탭만 종료
+browser.quit() # 전체 브라우저 종료
+
+
+# while(True):
+#     pass
 
 # # 로그인 버튼 클릭하기
 # elem = browser.find_element_by_class_name("link_login")
