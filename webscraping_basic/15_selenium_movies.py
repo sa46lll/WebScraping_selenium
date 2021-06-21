8ff21499dedc774f1b8a02f33fa523edd2e3e470
sa46lll <9812smh@naver.com>
2021-06-21 16:17:55 +0900

    Google Movies_bs4

---
+++ webscraping_basic/15_selenium_movies.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://play.google.com/store/movies/top"
+headers = {
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36",
+    "Accept-Language":"ko_KR, ko"
+    }
+
+res = requests.get(url, headers=headers)
+res.raise_for_status()
+soup = BeautifulSoup(res.text, "lxml")
+
+movies = soup.find_all("div", attrs={"class" : "ImZGtf mpg5gc"})
+print(len(movies))
+
+# with open("movie.html", "w", encoding="utf8") as f:
+#     # f.write(res.text)
+#     f.write(soup.prettify())
+
+for movie in movies:
+    title = movie.find("div", attrs={"class":"WsMG1c nnK0zc"}).get_text()
+    print(title)
