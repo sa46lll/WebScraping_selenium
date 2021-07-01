5529ea9090c01b85f391b4db10f1fd06a8fce4dc
sa46lll <9812smh@naver.com>
2021-07-01 14:02:21 +0900

    GM_Show sale price

--- webscraping_basic/16_selenium_movies_scroll.py
+++ webscraping_basic/16_selenium_movies_scroll.py
@@ -1,28 +1,3 @@
-# import requests
-# from bs4 import BeautifulSoup
-
-# url = "https://play.google.com/store/movies/top"
-# headers = {
-#     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36",
-#     "Accept-Language":"ko_KR, ko"
-#     }
-
-# res = requests.get(url, headers=headers)
-# res.raise_for_status()
-# soup = BeautifulSoup(res.text, "lxml")
-
-# movies = soup.find_all("div", attrs={"class" : "ImZGtf mpg5gc"})
-# print(len(movies))
-
-# # with open("movie.html", "w", encoding="utf8") as f:
-# #     # f.write(res.text)
-# #     f.write(soup.prettify()) //안되면 user-agent
-
-# for movie in movies:
-#     title = movie.find("div", attrs={"class":"WsMG1c nnK0zc"}).get_text()
-#     print(title)
-
-
 from selenium import webdriver
 import time
 browser = webdriver.Chrome()
@@ -31,7 +6,6 @@ browser.maximize_window()
 # 페이지 이동
 url = "https://play.google.com/store/movies/top"
 browser.get(url)
-time.sleep(3)
 
 # 지정한 위치로 스크롤 내리기
 # 모니터(해상도) 높이인 1080 위치로 스크롤 내리기
@@ -63,3 +37,41 @@ while True:
 
 print("스크롤 완료")
 
+
+import requests
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(browser.page_source, "lxml")
+
+# 여러개일 경우
+# movies = soup.find_all("div", attrs={"class" : ["ImZGtf mpg5gc", "Vpfmgd"]})
+movies = soup.find_all("div", attrs={"class" : "Vpfmgd"})
+
+print(len(movies))
+
+for movie in movies:
+    title = movie.find("div", attrs={"class":"WsMG1c nnK0zc"}).get_text()
+    # print(title)
+
+    # 할인 전 가격
+    original_price = movie.find("span", attrs={"class":"SUZt4c djCuy"})
+    if original_price:
+        original_price = original_price.get_text()
+    else:
+        # print(title, "<할인되지 않은 영화 제외>")
+        continue
+
+    # 할인된 가격
+    price = movie.find("span", attrs={"class":"VfPpfd ZdBevf i5DZme"}).get_text()
+
+    # 링크
+    link = movie.find("a", attrs={"class":"JC71ub"})["href"]
+    # 올바른 링크 : https://play.google.com/store/movies/top + link
+
+    print(f"제목 : {title}")
+    print(f"할인 전 금액 : {original_price}")
+    print(f"할인 후 금액 : {price}")
+    print("링크 : ", "https://play.google.com" + link)
+    print("-" * 100)
+
+browser.quit()
